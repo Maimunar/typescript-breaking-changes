6e4c7beb05e6109cd63a66fef60529634383a4fa
Aleksandar Ivanov <metalcho17@gmail.com>
2025-03-19 11:39:30 +0100

    finished types

--- 3groupMembersModel/type/code.ts
+++ 3groupMembersModel/type/code.ts
@@ -3,25 +3,17 @@ import { FeatureModel } from "../../model";
 export const Type: FeatureModel = {
   name: "Type",
   childrenRelation: "xor",
-  x: 522,
-  y: 75,
   children: [
     {
       name: "FunctionType",
-      x: 196,
-      y: 347,
       children: [
         {
           name: "parameters",
           parentRelation: "mandatory",
-          x: 123,
-          y: 444,
           children: [
             {
               parentRelation: "mandatory",
               name: "Type*",
-              x: 122,
-              y: 519,
             },
           ],
         },
@@ -29,18 +21,12 @@ export const Type: FeatureModel = {
           name: "returnType",
           parentRelation: "mandatory",
           childrenRelation: "xor",
-          x: 205,
-          y: 590,
           children: [
             {
               name: "Type",
-              x: 250,
-              y: 760,
             },
             {
               name: "TypePredicate",
-              x: 176,
-              y: 706,
             },
           ],
         },
@@ -48,26 +34,18 @@ export const Type: FeatureModel = {
     },
     {
       name: "TypeLiteral",
-      x: 660,
-      y: 288,
       children: [
         {
           name: "PropertySignature*",
           parentRelation: "mandatory",
-          x: 660,
-          y: 388,
           children: [
             {
               name: "key",
               parentRelation: "mandatory",
               childrenRelation: "xor",
-              x: 620,
-              y: 500,
               children: [
                 {
                   name: "StringLiteral",
-                  x: 436,
-                  y: 604,
                 },
                 {
                   name: "IndexSignature",
@@ -75,8 +53,6 @@ export const Type: FeatureModel = {
                     {
                       name: "keyName",
                       parentRelation: "mandatory",
-                      x: 375,
-                      y: 888,
                     },
                     {
                       name: "keyType",
@@ -85,31 +61,19 @@ export const Type: FeatureModel = {
                       children: [
                         {
                           name: "string",
-                          x: 508,
-                          y: 993,
                         },
                         {
                           name: "number",
-                          x: 572,
-                          y: 1041,
                         },
                         {
                           name: "symbol",
-                          x: 625,
-                          y: 981,
                         },
                         {
                           name: "TemplateLiteralType",
-                          x: 402,
-                          y: 1040,
                         },
                       ],
-                      x: 497,
-                      y: 877,
                     },
                   ],
-                  x: 445,
-                  y: 796,
                 },
                 {
                   name: "MappedType",
@@ -117,34 +81,24 @@ export const Type: FeatureModel = {
                     {
                       name: "Property",
                       parentRelation: "mandatory",
-                      x: 589,
-                      y: 680,
                     },
                     {
                       name: "TypeReference",
                       parentRelation: "mandatory",
-                      x: 640,
-                      y: 737,
                       children: [
                         {
                           name: "Type",
                           parentRelation: "mandatory",
-                          x: 640,
-                          y: 790,
                         },
                       ],
                     },
                   ],
-                  x: 683,
-                  y: 600,
                 },
               ],
             },
             {
               name: "Type",
               parentRelation: "mandatory",
-              x: 730,
-              y: 500,
             },
           ],
         },
@@ -152,178 +106,118 @@ export const Type: FeatureModel = {
     },
     {
       name: "ArrayType",
-      x: 370,
-      y: 293,
       children: [
         {
           name: "Type",
           parentRelation: "mandatory",
-          x: 370,
-          y: 350,
         },
       ],
     },
     {
       name: "TupleType",
-      x: 517,
-      y: 285,
       children: [
         {
           name: "Type*",
           parentRelation: "mandatory",
-          x: 517,
-          y: 350,
         },
       ],
     },
     {
       name: "UnionType",
-      x: 880,
-      y: 251,
       children: [
         {
           name: "Type",
-          x: 780,
-          y: 330,
           parentRelation: "mandatory",
         },
         {
           name: "secondType",
           parentRelation: "mandatory",
-          x: 950,
-          y: 330,
         },
       ],
     },
     {
       name: "IntersectionType",
-      x: 450,
-      y: 450,
       children: [
         {
           name: "Type",
-          x: 500,
-          y: 540,
           parentRelation: "mandatory",
         },
         {
           name: "secondType",
-          x: 350,
-          y: 540,
           parentRelation: "mandatory",
         },
       ],
     },
     {
       name: "TypeOperator",
-      x: 278,
-      y: 412,
       children: [
         {
           name: "Type",
           parentRelation: "mandatory",
-          x: 278,
-          y: 462,
         },
       ],
     },
     {
       name: "ConstructorType",
       childrenRelation: "xor",
-      x: 875,
-      y: 645,
       children: [
         {
           name: "LiteralType",
-          x: 779,
-          y: 754,
         },
         {
           name: "TypeKeyword",
           childrenRelation: "xor",
-          x: 1157,
-          y: 862,
           children: [
             {
               name: "string",
-              x: 1332,
-              y: 910,
             },
             {
               name: "number",
-              x: 1220,
-              y: 1040,
             },
             {
               name: "boolean",
-              x: 1332,
-              y: 1067,
             },
             {
               name: "any",
-              x: 1202,
-              y: 951,
             },
             {
               name: "never",
-              x: 1360,
-              y: 983,
             },
             {
               name: "object",
-              x: 1069,
-              y: 960,
             },
             {
               name: "symbol",
-              x: 1304,
-              y: 1014,
             },
             {
               name: "undefined",
-              x: 1114,
-              y: 1044,
             },
             {
               name: "unknown",
-              x: 1079,
-              y: 1001,
             },
             {
               name: "void",
-              x: 1272,
-              y: 984,
             },
           ],
         },
         {
           name: "IndexedAccessType",
-          x: 987,
-          y: 833,
           children: [
             {
               name: "ObjectType",
               parentRelation: "mandatory",
-              x: 987,
-              y: 888,
             },
           ],
         },
         {
           name: "ImportType",
-          x: 894,
-          y: 788,
         },
         {
           name: "TypeQuery",
-          x: 812,
-          y: 837,
           children: [
             {
               name: "VariableDeclaration",
               parentRelation: "mandatory",
-              x: 812,
-              y: 932,
             },
           ],
         },
--- 3groupMembersModel/type/full/code.ts
+++ 3groupMembersModel/type/full/code.ts
@@ -1,49 +1,36 @@
-import { FeatureModel } from "../../model";
+import { FeatureModel } from "../../../model";
 
 export const Type: FeatureModel = {
   name: "Type",
   childrenRelation: "xor",
-  x: 522,
-  y: 75,
   children: [
     {
       name: "BuildType",
       childrenRelation: "xor",
-      x: 343,
-      y: 159,
       children: [
         {
           name: "FunctionType",
-          x: 196,
-          y: 347,
           children: [
             {
               name: "parameters",
-              x: 123,
-              y: 444,
+              parentRelation: "mandatory",
               children: [
                 {
+                  parentRelation: "mandatory",
                   name: "BuildType*",
-                  x: 122,
-                  y: 519,
                 },
               ],
             },
             {
               name: "returnType",
+              parentRelation: "mandatory",
               childrenRelation: "xor",
-              x: 205,
-              y: 590,
               children: [
                 {
                   name: "BuildType",
-                  x: 250,
-                  y: 760,
                 },
                 {
                   name: "TypePredicate",
-                  x: 176,
-                  y: 706,
                 },
               ],
             },
@@ -51,25 +38,18 @@ export const Type: FeatureModel = {
         },
         {
           name: "TypeLiteral",
-          x: 660,
-          y: 288,
           children: [
             {
               name: "PropertySignature*",
-              x: 660,
-              y: 388,
+              parentRelation: "mandatory",
               children: [
                 {
                   name: "key",
                   parentRelation: "mandatory",
                   childrenRelation: "xor",
-                  x: 620,
-                  y: 500,
                   children: [
                     {
                       name: "StringLiteral",
-                      x: 436,
-                      y: 604,
                     },
                     {
                       name: "IndexSignature",
@@ -77,8 +57,6 @@ export const Type: FeatureModel = {
                         {
                           name: "keyName",
                           parentRelation: "mandatory",
-                          x: 375,
-                          y: 888,
                         },
                         {
                           name: "keyType",
@@ -87,62 +65,44 @@ export const Type: FeatureModel = {
                           children: [
                             {
                               name: "string",
-                              x: 508,
-                              y: 993,
                             },
                             {
                               name: "number",
-                              x: 572,
-                              y: 1041,
                             },
                             {
                               name: "symbol",
-                              x: 625,
-                              y: 981,
                             },
                             {
                               name: "TemplateLiteralType",
-                              x: 402,
-                              y: 1040,
                             },
                           ],
-                          x: 497,
-                          y: 877,
                         },
                       ],
-                      x: 445,
-                      y: 796,
                     },
                     {
                       name: "MappedType",
                       children: [
                         {
                           name: "Property",
-                          x: 589,
-                          y: 680,
+                          parentRelation: "mandatory",
                         },
                         {
                           name: "TypeReference",
-                          x: 640,
-                          y: 737,
+                          parentRelation: "mandatory",
                           children: [
                             {
                               name: "BuildType",
-                              x: 640,
-                              y: 790,
+                              parentRelation: "mandatory",
                             },
                           ],
                         },
                       ],
-                      x: 683,
-                      y: 600,
                     },
                   ],
                 },
                 {
                   name: "BuildType",
-                  x: 730,
-                  y: 500,
+                  parentRelation: "mandatory",
                 },
               ],
             },
@@ -150,182 +110,120 @@ export const Type: FeatureModel = {
         },
         {
           name: "ArrayType",
-          x: 370,
-          y: 293,
           children: [
             {
               name: "BuildType",
               parentRelation: "mandatory",
-              x: 370,
-              y: 350,
             },
           ],
         },
         {
           name: "TupleType",
-          x: 517,
-          y: 285,
           children: [
             {
               name: "BuildType*",
               parentRelation: "mandatory",
-              x: 517,
-              y: 350,
             },
           ],
         },
         {
           name: "UnionType",
-          x: 880,
-          y: 251,
           children: [
             {
               name: "BuildType",
-              x: 780,
-              y: 330,
               parentRelation: "mandatory",
             },
             {
               name: "secondBuildType",
               parentRelation: "mandatory",
-              x: 950,
-              y: 330,
             },
           ],
         },
         {
           name: "IntersectionType",
-          x: 450,
-          y: 450,
           children: [
             {
               name: "BuildType",
-              x: 500,
-              y: 540,
               parentRelation: "mandatory",
             },
             {
               name: "secondBuildType",
-              x: 350,
-              y: 540,
               parentRelation: "mandatory",
             },
           ],
         },
         {
           name: "TypeOperator",
-          x: 278,
-          y: 412,
           children: [
             {
               name: "BuildType",
-              x: 278,
-              y: 462,
+              parentRelation: "mandatory",
             },
           ],
         },
         {
           name: "ConstructorType",
-          x: 146,
-          y: 272,
-        },
-      ],
-    },
-    {
-      name: "ConstructorType",
-      childrenRelation: "xor",
-      x: 875,
-      y: 645,
-      children: [
-        {
-          name: "LiteralType",
-          x: 779,
-          y: 754,
-        },
-        {
-          name: "TypeKeyword",
           childrenRelation: "xor",
-          x: 1157,
-          y: 862,
           children: [
             {
-              name: "string",
-              x: 1332,
-              y: 910,
+              name: "LiteralType",
             },
             {
-              name: "number",
-              x: 1220,
-              y: 1040,
+              name: "TypeKeyword",
+              childrenRelation: "xor",
+              children: [
+                {
+                  name: "string",
+                },
+                {
+                  name: "number",
+                },
+                {
+                  name: "boolean",
+                },
+                {
+                  name: "any",
+                },
+                {
+                  name: "never",
+                },
+                {
+                  name: "object",
+                },
+                {
+                  name: "symbol",
+                },
+                {
+                  name: "undefined",
+                },
+                {
+                  name: "unknown",
+                },
+                {
+                  name: "void",
+                },
+              ],
             },
             {
-              name: "boolean",
-              x: 1332,
-              y: 1067,
+              name: "IndexedAccessType",
+              children: [
+                {
+                  name: "ObjectType",
+                  parentRelation: "mandatory",
+                },
+              ],
             },
             {
-              name: "any",
-              x: 1202,
-              y: 951,
+              name: "ImportType",
             },
             {
-              name: "never",
-              x: 1360,
-              y: 983,
-            },
-            {
-              name: "object",
-              x: 1069,
-              y: 960,
-            },
-            {
-              name: "symbol",
-              x: 1304,
-              y: 1014,
-            },
-            {
-              name: "undefined",
-              x: 1114,
-              y: 1044,
-            },
-            {
-              name: "unknown",
-              x: 1079,
-              y: 1001,
-            },
-            {
-              name: "void",
-              x: 1272,
-              y: 984,
-            },
-          ],
-        },
-        {
-          name: "IndexedAccessType",
-          x: 987,
-          y: 833,
-          children: [
-            {
-              name: "ObjectType",
-              x: 987,
-              y: 888,
-            },
-          ],
-        },
-        {
-          name: "ImportType",
-          x: 894,
-          y: 788,
-        },
-        {
-          name: "TypeQuery",
-          x: 812,
-          y: 837,
-          children: [
-            {
-              name: "VariableDeclaration",
-              x: 812,
-              y: 932,
+              name: "TypeQuery",
+              children: [
+                {
+                  name: "VariableDeclaration",
+                  parentRelation: "mandatory",
+                },
+              ],
             },
           ],
         },
@@ -334,49 +232,36 @@ export const Type: FeatureModel = {
     {
       name: "AuxilaryType",
       childrenRelation: "xor",
-      x: 1335,
-      y: 203,
       children: [
         {
           name: "TypeReference",
-          x: 1400,
-          y: 320,
           children: [
             {
               name: "typeParameter*",
               parentRelation: "mandatory",
-              x: 1262,
-              y: 379,
             },
             {
               name: "returnType",
               parentRelation: "optional",
               childrenRelation: "xor",
-              x: 1416,
-              y: 379,
               children: [
                 {
                   name: "BuildType",
-                  x: 1470,
-                  y: 466,
                 },
                 {
                   name: "ConditionalType",
-                  x: 1300,
-                  y: 466,
                   children: [
                     {
                       name: "typeParameter",
-                      x: 1193,
-                      y: 564,
+                      parentRelation: "mandatory",
                     },
                     {
                       name: "extendsConditionalObject",
+                      parentRelation: "mandatory",
+                      childrenRelation: "xor",
                       children: [
                         {
                           name: "BuildType",
-                          x: 1307,
-                          y: 710,
                         },
                         {
                           name: "InferType",
@@ -384,26 +269,18 @@ export const Type: FeatureModel = {
                             {
                               name: "TypeReference",
                               parentRelation: "mandatory",
-                              x: 1152,
-                              y: 770,
                             },
                           ],
-                          x: 1152,
-                          y: 710,
                         },
                       ],
-                      x: 1307,
-                      y: 630,
                     },
                     {
                       name: "ThenType",
-                      x: 1389,
-                      y: 585,
+                      parentRelation: "mandatory",
                     },
                     {
                       name: "ElseType",
-                      x: 1417,
-                      y: 534,
+                      parentRelation: "mandatory",
                     },
                   ],
                 },
@@ -413,18 +290,14 @@ export const Type: FeatureModel = {
         },
         {
           name: "TypePredicate",
-          x: 1200,
-          y: 300,
           children: [
             {
               name: "parameterName",
-              x: 1131,
-              y: 418,
+              parentRelation: "mandatory",
             },
             {
               name: "BuildType",
-              x: 950,
-              y: 418,
+              parentRelation: "mandatory",
             },
           ],
         },
